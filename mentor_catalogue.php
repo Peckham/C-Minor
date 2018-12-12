275d988baa941ddd8084dc6eaae7a2c32d7394b9
Tiara Tan <tt1506@nyu.edu>
2018-12-12 23:48:58 +0800

    Mentor Catalogue CSS

--- mentor_catalogue.php
+++ mentor_catalogue.php
@@ -54,29 +54,52 @@ try {
     }
 </style>
 
-<div class="page-header header">
-    <h1>Mentor Catalogue</h1>
-</div>
+<div class="container">
 
-<div class="bodyText">
-    <table>
-        <thead class="tableHeader">
-        <tr>
-            <!-- <th>#</th> -->
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Email Address</th>
-        </tr>
-        </thead>
-        <tbody>
+      <!-- Page Heading -->
+      <h1 class="my-4 white">Mentor Catalogue</h1>
+
+      <div class="row">
         <?php foreach ($result as $row) : ?>
-            <tr>
-                <!-- <td><?php echo escape($row["id"]); ?></td> -->
-                <td><?php echo escape($row["fname"]); ?></td>
-                <td><?php echo escape($row["lname"]); ?></td>
-                <td><?php echo escape($row["email"]); ?></td>
-            </tr>
+            <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
+              <div class="card">
+                <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                <div class="card-body">
+                  <h4 class="card-title">
+                    <a href="#"><?php echo escape($row["fname"]);?> <?php echo escape($row["lname"]);?></a>
+                  </h4>
+                  <p class="card-text"><?php echo escape($row["email"]); ?></p>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur eum quasi sapiente nesciunt? Voluptatibus sit, repellat sequi itaque deserunt, dolores in, nesciunt, illum tempora ex quae? Nihil, dolorem!</p>
+                </div>
+              </div>
+            </div>
         <?php endforeach; ?>
-        </tbody>
-    </table>
+    </div>
+
+      <!-- Pagination -->
+      <ul class="pagination justify-content-center">
+        <li class="page-item">
+          <a class="page-link" href="#" aria-label="Previous">
+            <span aria-hidden="true">&laquo;</span>
+            <span class="sr-only">Previous</span>
+          </a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="#">1</a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="#">2</a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="#">3</a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="#" aria-label="Next">
+            <span aria-hidden="true">&raquo;</span>
+            <span class="sr-only">Next</span>
+          </a>
+        </li>
+      </ul>
+
+    </div>
 </div>
